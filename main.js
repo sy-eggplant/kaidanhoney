1fe085096d3f30ff11c1011d4f7222d0986b7495
shoko <sy.eggplant@gmail.com>
2015-06-27 18:25:56 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function talk(){
+	window.location.href = "talkPeople.html";
+}
+function listen(){	
+	window.location.href = "listenPeople.html";
+}
+function tap(){	
+	window.location.href = "index.html";
+}
